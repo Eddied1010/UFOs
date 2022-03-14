feda13a087e885e4df120d252f814aad4505ab13
Eddie D <edavidson1010@gmail.com>
2022-03-13 23:46:47 -0400

    updates to final

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # UFOs
+The Module 11 Challenge Project on UFO analysis is used to provide in-depth Analysis UFO sightings. By sourcing an array of data that list UFO sightings.
+This project allows a user to search that data based on several filters such as date, city, state, country and UFO shapes.  
+
+Users can search the database based on the listed filters that will output rows of UFO analysis based on their filtered requirement.  User can also clear his previous searches, modify or save searches.   
+
+The Truth is Out there!  Untilize Dana's UFO anaylsis find the details you need to define your truth.
+
+
+
--- static/js/app.js
+++ static/js/app.js
@@ -24,21 +24,51 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-
+let date = d3.select("#datetime").property("value");
+let city = d3.select("#city").property("value");
+let state = d3.select("#State").property("value");
+let country = d3.select("#country").property("value");
+let shape = d3.select("#shape").property("value")
+let filteredData = tableData;
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
-
-    // 4a. Save the element that was changed as a variable.
-
-    // 4b. Save the value that was changed as a variable.
-
-    // 4c. Save the id of the filter that was changed as a variable.
-
+  if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `entered` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  } 
+    else if (city){
+      filteredData = filteredData.filter(row => row.city === city);
+    }     
+      else if (state){
+        filteredData = filteredData.filter(row => row.state === state);
+      }
+        else if (country){
+          filteredData = filteredData.filter(row => row.country === country);
+        }
+          else if (shape){
+            filteredData = filteredData.filter(row => row.shape === shape);
+          }
+    else {
+      console.log("Not found")
+    }
   
+  };
+    // 4a. Save the element that was changed as a variable.
+let changeElement = filteredData.filter(filteredData);
+    // 4b. Save the value that was changed as a variable.
+let changeValue = filteredData.filter(values);
+    // 4c. Save the id of the filter that was changed as a variable.
+let changeID = filteredData.filter("id");
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
- 
+ if (changeValue) {
+   filterValue[changeID] = changeValue;
+ }
+ else {
+   delete filterValue[changeID];
+ }
   
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
@@ -50,17 +80,19 @@ function updateFilters() {
   
     // 8. Set the filtered data to the tableData.
     
-  
+  let filteredData = tableData;
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
+    filteredData=filteredData.forEach((filterValue)=> {
+      filterValue =>filterValue.id == "id"});
+      console.log(filteredData)
     
   
     // 10. Finally, rebuild the table using the filtered data
     
-  }
+  buildTable(filteredData)
   
   // 2. Attach an event to listen for changes to each filter
-  
   
   // Build the table when the page loads
   buildTable(tableData);
